48902be8bc8ec78da56eae3f45074c23d90c9384
mj305 <maria.j.beckles@gmail.com>
2020-10-28 14:20:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [color, setColor] =useState()
+  const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+  const randomColors = e => {
+    const len = colors.length;
+    setColor(Math.floor(Math.random() * len));
+  }
+
+  const [quote, setQuote] = useState({
+    quotes: [],
+  })
+  const fetchQuote = () => {
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then(response => response.json())
+    .then(data => setQuote(data))
+    .catch(error => console.log("Error: ", error))   
+  }
+  const randomQuote = e => {
+    const len = fetchQuote.length;
+    setColor(Math.floor(Math.random() * len));
+  }
+
+
+  const results = quote.quotes.map((value) => {
+    return (
+      <div className="text-container" style={{backgroundColor: colors[color]}} >
+        <div className="text-wrapper" style={{color: colors[color]}} >
+          <p>"{value.quote}"</p>
+          <p className="text-author" >- {value.author} </p>
+          <button className="button" style={{backgroundColor: colors[color]}} onClick={randomColors} >New Quote</button>
+        </div>
+      </div>
+    )
+  })
+
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {results}
+       <div className="buttons-container">
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
