93d0e0fd3a1b7cf46a5ff436cab389ba0d78ff77
mj305 <maria.j.beckles@gmail.com>
2020-10-30 17:59:29 -0400

    state for random quote

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import './App.css';
 function App() {
 
   const [color, setColor] =useState()
+
+  const [quote, setQuote] = useState({
+    quotes: [
+      {
+      quote: "",
+      author: ""
+      },
+    ],
+  })
+
+  const [quoteIndex, setQuoteIndex] = useState(0)
+
+
   const colors = [
     '#16a085',
     '#27ae60',
@@ -18,29 +31,39 @@ function App() {
     '#77B1A9',
     '#73A857'
   ];
+
+  const randomQuote = e => {
+    const len = quote.quotes.length;
+    setQuoteIndex(Math.floor(Math.random() * len));
+    console.log(len)
+  }
+
   const randomColors = e => {
     const len = colors.length;
     setColor(Math.floor(Math.random() * len));
+
+    randomQuote()
   }
 
-  const [quote, setQuote] = useState({
-    quotes: [],
-  })
+  const value = quote.quotes[quoteIndex]
+  console.log(value)
+
   const fetchQuote = () => {
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
     .then(response => response.json())
     .then(data => setQuote(data))
     .catch(error => console.log("Error: ", error))   
   }
-/*   const randomQuote = e => {
-    const len = fetchQuote.length;
-    setColor(Math.floor(Math.random() * len));
-  }
- */
 
-  const results = quote.quotes.map((value) => {
-    return (
-      <div className="container" style={{backgroundColor: colors[color]}} >
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+  
+
+
+  return (
+    <div>
+       <div className="container" style={{backgroundColor: colors[color]}} >
         <div className="wrapper" style={{color: colors[color]}} >
           <p>"{value.quote}"</p>
           <p>- {value.author} </p>
@@ -57,17 +80,6 @@ function App() {
 
         </div>
       </div>
-    )
-  })
-
-  useEffect(() => {
-    fetchQuote()
-  }, [])
-
-
-  return (
-    <div>
-      {results}
        <div className="buttons-container">
       </div>
     </div>
